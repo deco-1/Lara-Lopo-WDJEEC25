3cd78277d2c360e0096c515c3443ef74983afe67
deco <andrefsantos@skiff.com>
2024-04-25 00:51:55 +0100

    check everything carefully and learn

--- src/App.vue
+++ src/App.vue
@@ -1,141 +1,220 @@
 <script setup>
 import { ref } from 'vue'
-let count = ref(0);
-let message = "Happy Birthday to Shrek 1 Movie!";
-let showMessage = ref(false);
-let answer = "Ducks are very cute therefore make people happy, and it's scientifically proven that happy people are more productive in the workplace."
-let showAnswer = ref(false)
+const count = ref(0);
+const showAnswer = ref(false);
+const showFunFact = ref(false);
+
+const today = new Date();
+const dateModal = ref("");
+
+const date = ref({
+  year: today.getFullYear().toString(2),
+  month: (today.getMonth() + 1).toString(2),
+  day: today.getDate().toString(2),
+});
+
+function dateCallback() {
+  const dateArr = ["3", "3", "2"]
+  
+  date.value.year = dateArr[0].toString(2);
+  date.value.month = dateArr[1].toString(2);
+  date.value.day = dateArr[2].toString(2);
+}
 </script>
 
 <template>
+  <body>
+    <div class="carousel-wrapper">
+      <div class="carousel">
+        <button class="slide-move" @click="count = (count + 3) % 4">&lt;</button>
+        <div class="slide">
 
-  <slides>
-    <button class="MoveBtn" @click="count = (count + 3)%4"><</button>
-
-    <slideContainer :class="{ 'slide3Background': count === 3 }">
-        <div class="fadeIn" style="font-size: 250%; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; " v-if="count===0">Why are ducks important to the economy?</div>
-        <div class="fadeIn" style="font-size: 250%; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; " v-else-if="count===1">10110-100-11111010001</div>
-        <div class="fadeIn" v-else-if="count===2">
-          <slide2Content>
-            <p style="font-size: 200%; font-weight: bold;"><span style="font-style: italic; font-weight: bold;">Lorem Ipsum</span> is simply dummy text of the printing and typesetting industry.</p>
-            <p style="font-size: 150%;"><span style="font-style: italic;">Lorem Ipsum</span> has been the industry's standard dummy text </p>
-            <p style="font-size: 100%;">It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-          </slide2Content>
-        </div>
-        <div class="fadeIn" v-else-if="count===3"></div>
-    </slideContainer>
-
-    <button class="MoveBtn" @click="count = (count + 1)%4">></button>
-  </slides>
+          <div class="slide0" v-if="count === 0">
+            <h1>Why are ducks important to the economy?</h1>
+          </div>
   
-  <br>
-  <btnContainer>
-    <button class="SlideBtn" @click="count = 0"></button>
-    <button class="SlideBtn" @click="count = 1"></button>
-    <button class="SlideBtn" @click="count = 2"></button>
-    <button class="SlideBtn" @click="count = 3"></button>
-  </btnContainer>
-  <br>
+          <div class="slide1" v-else-if="count === 1">
+            <div class="slide1-top">
+              <p>Input a date:</p>
+              <input type="date" v-model="dateModal" @input="dateCallback">
+            </div>
+            <p>{{ date.day }} - {{ date.month }} - {{ date.year }}</p>
+          </div>
+  
+          <div class="slide2" v-else-if="count === 2">
+            <h1>
+              <bold><i>Lorem Ipsum</i>is simply dummy text of the printing and typesetting industry.</bold>
+            </h1>
+            <h2>Lorem Ipsum has been the industry's standard dummy text</h2>
+            <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining
+              essentially
+              unchanged. It was popularized in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+              passages, and more recently with desktop publishing software like Aldus PageMaker including versions of
+              Lorem
+              Ipsum.</p>
+          </div>
+  
+          <div class="slide3" v-else-if="count === 3">
+            <img src="@/assets/challenge.png" alt="WebDev">
+          </div>
 
-  <paragraph>
-      <p1 class="fadeIn" v-if="count===0">
-        <button v-if="!showAnswer" @click="showAnswer=true">show answer</button>
-        <msg v-if="showAnswer" style="font-size: 17px; font-weight: lighter; font-family:Arial, Helvetica, sans-serif; opacity: 30%;">{{ answer }}</msg>
-      </p1>
-      <p2 class="fadeIn" v-else-if="count===1"> 
-        <p>22/04/2001</p> 
-        <button v-if="!showMessage" @click="showMessage=true">fun fact</button>
-        <msg v-if="showMessage" style="font-size: 17px; font-weight: lighter; font-family:Arial, Helvetica, sans-serif; opacity: 30%;">{{ message }}</msg>
-      </p2>
-  </paragraph>
+        </div>
+        <button class="slide-move" @click="count = (count + 1) % 4">&gt;</button>
+      </div>
+
+      <div class="carousel-pagination">
+        <button @click="count = 0"></button>
+        <button @click="count = 1"></button>
+        <button @click="count = 2"></button>
+        <button @click="count = 3"></button>
+      </div>
+
+      <div class="under-text">
+        <template v-if="count === 0">
+          <button @click="showAnswer = true" v-if="!showAnswer">show answer</button>
+          <p v-if="showAnswer">Ducks are very cute therefore make people happy, and it's scientifically proven that
+            happy people are more productive in the workplace.</p>
+        </template>
+        <template v-else-if="count === 1">
+          <p>22/04/2001</p>
+          <button @click="showFunFact = true" v-if="!showFunFact">fun fact</button>
+          <p v-if="showFunFact">Happy Birthday to Shrek 1 Movie!</p>
+        </template>
+      </div>
+    </div>
+
+
+  </body>
 </template>
 
 <style>
-  slides {
-    margin-top: 10vh;
-    display: flex;
-    gap: 30px;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-  }
-  slideContainer {
-    background-color: white;
-    width: 750px; 
-    height: 500px;
-    position: relative;
-    border-radius: 5%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    /*transition: opacity 0.5s ease;*/
-  }
-  btnContainer {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    gap: 10px;
-  }
-  .SlideBtn {
-    background-color: #008ECC;
-    border: none;
-    opacity: 30%;
-    width: 10px;
-    height: 10px;
-    border-radius: 50%;
-  }
-  .MoveBtn {
-    height: 35px;
-    width: 35px;
-    border-radius: 70%;
-    border: none;
-    background-color: #b9deee;
-    color:#003c56;
-    font-weight: bolder;
-    font-size: larger;
-  }
-  div {
-    color:black;
-  }
-  paragraph {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    align-content: center;
-    text-align: center;
-    color:#b9deee;
-    font-size: 150%;
-    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-  }
-  slide2Content {
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    margin-left: 50px;
-    margin-right:  50px;
-  }
-  .slide3Background {
-      background-image: url('assets/challenge.png');
-      background-size: cover;
-      background-position: center;
-    }
-  .fadeIn {
-	opacity: 1;
-	animation-name: fadeInOpacity;
-	animation-iteration-count: 1;
-	animation-timing-function: ease-in;
-	animation-duration: 0.6s;
-  }
-
-@keyframes fadeInOpacity {
-	0% {
-		opacity: 0;
-	}
-	100% {
-		opacity: 1;
-	}
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 
+h1,
+h2,
+p {
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+body {
+  height: 100svh;
+  width: 100svw;
+  color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.carousel-wrapper {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.carousel {
+  display: flex;
+  align-items: center;
+  margin: 0 2rem;
+  gap: 15px;
+}
+
+.slide-move {
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  border: none;
+  background-color: #b9deee;
+  color: #003c56;
+  font-weight: 700;
+  cursor: pointer;
+}
+
+.slide-move:hover {
+  scale: 1.1;
+}
+
+.slide {
+  max-width: 750px;
+  width: 100vw;
+  height: 500px;
+  background-color: white;
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+}
+
+.slide1 {
+  font-size: 2rem;
+}
+
+.slide1-top {
+  display: flex;
+  gap: 1ch;
+  margin-bottom: 0.5rem;
+}
+
+.slide2 {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 2rem;
+}
+
+.slide3>img {
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+}
+
+.carousel-pagination {
+  margin: 1rem 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+}
+
+.carousel-pagination>button {
+  background-color: #008ECC;
+  border: none;
+  opacity: 30%;
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+.under-text {
+  margin: 0 auto;
+  position: absolute;
+  text-align: center;
+  color: #b9deee4d;
+  font-size: 1.3rem;
+  top: 100%;
+}
+
+:is(.slide, .under-text)>div {
+  opacity: 1;
+  animation-name: fadeInOpacity;
+  animation-iteration-count: 1;
+  animation-timing-function: ease-in;
+  animation-duration: 0.6s;
+}
+
+@keyframes fadeInOpacity {
+  0% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
+}
 </style>
